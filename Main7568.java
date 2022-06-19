7cf313e860e1f301732f62b8befabf77e9e0e23a
jjw213 <ssvsxx@naver.com>
2022-06-19 20:45:18 +0900

    Main7568 & 1106

---
+++ Main7568.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main7568 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()); // 사람
+        int[][] name = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            name[i][0] = Integer.parseInt(st.nextToken()); // 몸무게
+            name[i][1] = Integer.parseInt(st.nextToken()); // 키
+        }
+//        Arrays.sort(name, (o1, o2) -> { // wrong
+//            if(o1[0] == o2[0]){
+//                return Integer.compare(o1[1], o2[1]);
+//            } else {
+//                return Integer.compare(o1[0], o2[0]);
+//            }
+//        });
+//
+//        for (int j=n-1;j>=0;j--){
+//            if(name[j][1]>name[j-1][1]){
+//
+//            }
+//        }
+        int[] cnt= new int[n];
+        Arrays.fill(cnt,1);
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                if (name[j][0] < name[k][0]) {
+                    if (name[j][1] < name[k][1])
+                        cnt[j]++;
+                }
+            }
+        System.out.println(cnt[j]);
+        }
+    }
+}
